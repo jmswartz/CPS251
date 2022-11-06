9269a720b038f3ec16efd49454076e97c82d6ca2
Jaron Swartz <jmswartz@wccnet.edu>
2022-11-06 16:37:06 -0500

    updates

---
+++ Assignments/NavigationDemo/app/src/main/java/com/example/navigationdemo/ui/main/MainFragment.kt
@@ -0,0 +1,74 @@
+package com.ebookfrenzy.navigationdemo.ui.main
+
+import android.app.Activity
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import com.ebookfrenzy.navigationdemo.R
+import androidx.navigation.Navigation
+import com.ebookfrenzy.navigationdemo.SecondFragment
+import com.ebookfrenzy.navigationdemo.databinding.MainFragmentBinding
+
+
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+
+    private lateinit var viewModel: MainViewModel
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.button1.setOnClickListener {
+            val action: MainFragmentDirections.MainToSecond  =  MainFragmentDirections.mainToSecond()
+            action.setMessage("Image 1")
+
+
+
+            Navigation.findNavController(it).navigate(action)
+
+
+        }
+        binding.button2.setOnClickListener {
+            val action: MainFragmentDirections.MainToSecond  =  MainFragmentDirections.mainToSecond()
+            action.setMessage("Image 2")
+
+
+            Navigation.findNavController(it).navigate(action)
+
+
+        }
+        binding.button3.setOnClickListener {
+            val action: MainFragmentDirections.MainToSecond  =  MainFragmentDirections.mainToSecond()
+            action.setMessage("Image 3")
+
+            Navigation.findNavController(it).navigate(action)
+
+
+        }
+    }
+
+}
